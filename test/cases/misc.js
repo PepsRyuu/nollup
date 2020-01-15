1ead8bb859f1e3e37cf550a0ad0649a7e975d219
Paul Sweeney <paul-sweeney@outlook.ie>
2020-01-15 18:34:20 +0000

    User friendly error for module not found

---
+++ test/cases/misc.js
@@ -0,0 +1,91 @@
+let { nollup, fs, expect } = require('../nollup');
+
+describe ('Misc', () => {
+    it ('should not throw error if using require with string in CJS format', async () => {
+        fs.stub('./src/main.js', () => 'require("path")');
+
+        let bundle = await nollup({
+            input: './src/main.js'
+        });
+
+        let { output } = await bundle.generate({ format: 'cjs' });
+        expect(() => eval(output[0].code)).not.to.throw();
+        fs.reset();
+    });
+
+    it ('should throw error if using require with string in CJS format and it doesn\'t exist', async () => {
+        fs.stub('./src/main.js', () => 'require("fake")');
+
+        let bundle = await nollup({
+            input: './src/main.js'
+        });
+
+        let { output } = await bundle.generate({ format: 'cjs' });
+        expect(() => eval(output[0].code)).to.throw('Cannot find module');
+        fs.reset();
+    });
+
+    it ('should throw error if using require with string in non-CJS format', async () => {
+        fs.stub('./src/main.js', () => 'require("hello")');
+
+        let bundle = await nollup({
+            input: './src/main.js'
+        });
+
+        let { output } = await bundle.generate({ format: 'esm' });
+        expect(() => eval(output[0].code)).to.throw('Module not found: hello');
+        fs.reset();
+    });
+
+    it ('should not throw error if using require on bundled id', async () => {
+        let executed = false;
+        fs.stub('./src/main.js', () => 'if (executed === false) {executed = true; require(0); }');
+
+        let bundle = await nollup({
+            input: './src/main.js'
+        });
+
+        let { output } = await bundle.generate({ format: 'esm' });
+        expect(() => eval(output[0].code)).not.to.throw();
+        expect(executed).to.be.true;
+        fs.reset();
+    });
+
+    it ('should not throw error if using require on bundled id in CJS format', async () => {
+        let executed = false;
+        fs.stub('./src/main.js', () => 'if (executed === false) {executed = true; require(0); }');
+
+        let bundle = await nollup({
+            input: './src/main.js'
+        });
+
+        let { output } = await bundle.generate({ format: 'cjs' });
+        expect(() => eval(output[0].code)).not.to.throw();
+        expect(executed).to.be.true;
+        fs.reset();
+    });
+
+    it ('should throw error if using require and passing unrecognised id', async () => {
+        fs.stub('./src/main.js', () => 'require(1)');
+
+        let bundle = await nollup({
+            input: './src/main.js'
+        });
+
+        let { output } = await bundle.generate({ format: 'esm' });
+        expect(() => eval(output[0].code)).to.throw('Module not found: 1');
+        fs.reset();
+    });
+
+    it ('should throw error if using require and passing unrecognised id in CJS format', async () => {
+        fs.stub('./src/main.js', () => 'require(1)');
+
+        let bundle = await nollup({
+            input: './src/main.js'
+        });
+
+        let { output } = await bundle.generate({ format: 'cjs' });
+        expect(() => eval(output[0].code)).to.throw('Module not found: 1');
+        fs.reset();
+    });
+});
